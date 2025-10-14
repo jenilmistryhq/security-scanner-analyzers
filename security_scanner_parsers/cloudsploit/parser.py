69457f382c8ef5b69619543d3f9fe2c81837c26e
jenilmistryhq <26339174+jenilmistryhq@users.noreply.github.com>
2025-10-14 11:20:52 +0000

    refactor code

--- security_scanner_parsers/cloudsploit/parser.py
+++ security_scanner_parsers/cloudsploit/parser.py
@@ -1,19 +1,18 @@
 from security_scanner_parsers.utils import load_json, generate_report, send_to_slack
 
 
-def nuclei_count_field(data, field_name, skip_values=None):
+def cloudsploit_count_field(data, field_name, skip_values=["ok"]):
     count_dict = {}
     for item in data:
-        value = item.get("info", {}).get(field_name, "unknown").lower()
+        value = item.get(field_name, "unknown").lower()
         if skip_values and value in skip_values:
             continue
         count_dict[value] = count_dict.get(value, 0) + 1
     return count_dict
 
-
 def main(file_path, slack_url):
     data = load_json(file_path)
-    severity_count = nuclei_count_field(data, "severity")
+    status_count = cloudsploit_count_field(data, "status")
 
-    report = generate_report(":rocket: Severity Report", severity_count)
+    report = generate_report(":cloud: CloudSploit Status Report", status_count)
     send_to_slack(slack_url, report)
